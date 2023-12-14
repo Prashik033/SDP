fd2efc14a3d8e4c0d2d6e0adac69d2b4e57c4d53
Prashik033 <141120806+Prashik033@users.noreply.github.com>
2023-12-14 10:55:33 +0530

    Add files via upload

---
+++ Super_callmethod.java
@@ -0,0 +1,24 @@
+public class Super_callmethod {
+  public double calculateEMI(double principal) {
+    double simpleInterest = (principal*8.5*5) / 100;
+    double emi = (simpleInterest+principal)/5;
+    return emi;
+    }
+}
+           
+    class HomeLoan extends Loan {
+        public double calculateEMI(double principal) {
+            int additionaltax = 200;
+            double emi = super.calculateEMI(principal);      //calling super class method
+            return emi + additionaltax;
+        }
+    }
+           
+    class ExecuteLoan {
+        public static void main(String[] args) {
+            Loan loan = null;
+            loan =  new HomeLoan();                 // Runtime polymorphism
+             double hloan = loan.calculateEMI(2000000);
+             System.out.println("Home loan emi per year..."+ hloan);
+        }
+    }
